78ae86c0559acdba3c963e7cede80f860c0ba23b
Radu Matei <radu@radu-matei.com>
2018-12-10 18:09:15 +0200

    Remove custom start script from the container image and update doc

--- draft.sh
+++ draft.sh
@@ -1,2 +1,2 @@
 draft up --skip-image-push
-brig run -f brigade.js deislabs/inner-loop -e image_push -p payload.json
+brig run -f brigade.js brigade-306a0cfa88df2effac00b4e205a848960fd9d7c8da00340fecda7f -e image_push -p payload.json
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ "" == ""$BRIGADE_PROJECT_ID ]; then
-  node src/index.js
-  exit
-fi
-
-cp /app/brigade.js $BRIGADE_WORKSPACE/brigade.js
